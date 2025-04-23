02052b7fc93f4127faf4902401a8940932030098
Mustafa Kazmi <76160433+MustafaKazmi@users.noreply.github.com>
2025-04-23 18:10:12 +0530

    Add files via upload

---
+++ course.py
@@ -0,0 +1,15 @@
+#TASK 1: 
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# TASK 2:
+
+first_name = input("Enter your First name: ")
+last_name = input("Enter your Last name: ")
+
+print("Hello ", first_name + " " + last_name, "Welcome Back.")
